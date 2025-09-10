75f73414457e3486c6967bcc4fb029a7df76a7e6
Aatif Akhter <aatif912@gmail.com>
2025-09-10 23:13:01 +0200

    Create SetupUbuntu.sh

---
+++ SetupUbuntu.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install curl
+sudo apt install git
+
+#zsh first and then oh my zsh
+sudo apt install zsh
+https://ohmyz.sh/#install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
