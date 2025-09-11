fb222b0c4e3777bf35f567e163760918cf419de2
Aatif Akhter <aatif912@gmail.com>
2025-09-11 14:50:18 +0200

    Update and rename SetupUbuntu.sh to SetupUbuntu24.04.sh

--- SetupUbuntu24.04.sh
+++ SetupUbuntu24.04.sh
@@ -31,3 +31,12 @@ https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
 #wizcli
 https://docs.wiz.io/docs/set-up-wiz-cli#download-wiz-cli-to-a-linux-mac-or-windows-host
+
+#pgadmin
+sudo apt update && sudo apt upgrade -y
+sudo apt install postgresql postgresql-contrib -y
+curl -fsSL https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+sudo apt install pgadmin4-desktop
+
+
